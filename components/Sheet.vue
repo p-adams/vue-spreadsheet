81c2cc8fb35fa1fd74e4c279ea34e76ef4ac07b3
User <paadams@nmu.edu>
2017-08-11 14:02:50 -0400

    updated Sheet component

--- components/Sheet.vue
+++ components/Sheet.vue
@@ -2,12 +2,18 @@
   <div>
      <table>
         <thead>
+          <tr>
+            <th v-for="(col, index) in cols" :key="index">
+              {{col}}
+            </th>
+          </tr>
         </thead>
         <tbody>
-          {{selected}}
           <tr v-for="(row, index) in rows" :key="index">
             <td v-for="(col, index) in cols" :key="index">
-              <div @click="selectCell(row, col)">
+              <div
+                  :class="{'selected' : cellSelected(row, col)}"
+                  @mousedown="selectCell(row, col)">
                 <input>
               </div>
             </td>
@@ -26,25 +32,50 @@ export default {
   data () {
     return {
       selected: '',
+      isSelected: false,
       rows: [],
       cols: null
     }
   },
-  methods:{
+  methods: {
     initCols () {
-      this.cols = "abcdef"
-                    .toUpperCase()
-                    .split('')
+      this.cols = 'abcdef'.toUpperCase().split('')
     },
     initRows () {
-      for(let i = 0; i <= 6; i++) {
+      for (let i = 1; i <= 6; i++) {
         this.rows.push(i)
       }
     },
     selectCell (row, col) {
       this.selected = row + col
+      this.isSelected = true
+      this.rows[row] = this.isSelected
+      this.cols[col] = this.isSelected
+      // alert(this.selected)
+    },
+    cellSelected (row, col) {
+      return (this.rows[row] === this.isSelected && this.cols[col] === this.isSelected)
     }
   }
 }
 </script>
+<style scoped>
+  table {
+    width: 50%;
+  }
+  table, th, td {
+    border: 1px solid #bdbdbd;
+    border-collapse: collapse;
+  }
+  th {
+    background: #00b0ff;
+    color: #e1f5fe;
+  }
+  th, td {
+    padding: 5px;
+  }
+  .selected {
+    background: red;
+  }
+</style>
 
