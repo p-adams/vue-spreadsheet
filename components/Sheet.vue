6a83db9e9c53fcd78eaee3b3a589314c27452a91
User <paadams@nmu.edu>
2017-08-11 01:53:36 -0400

    added table to Sheet component

--- components/Sheet.vue
+++ components/Sheet.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-      sheet component
+     <table>
+        <thead>
+        </thead>
+        <tbody>
+          {{selected}}
+          <tr v-for="(row, index) in rows" :key="index">
+            <td v-for="(col, index) in cols" :key="index">
+              <div @click="selectCell(row, col)">
+                <input>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+     </table>
   </div>
 </template>
 <script>
 export default {
-  name: 'sheet'
+  name: 'sheet',
+  created () {
+    this.initCols()
+    this.initRows()
+  },
+  data () {
+    return {
+      selected: '',
+      rows: [],
+      cols: null
+    }
+  },
+  methods:{
+    initCols () {
+      this.cols = "abcdef"
+                    .toUpperCase()
+                    .split('')
+    },
+    initRows () {
+      for(let i = 0; i <= 6; i++) {
+        this.rows.push(i)
+      }
+    },
+    selectCell (row, col) {
+      this.selected = row + col
+    }
+  }
 }
 </script>
 
