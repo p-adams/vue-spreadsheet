ef1fb27f29919cc938bd2ff0d98a0c32241ff2eb
User <paadams@nmu.edu>
2017-08-16 14:24:54 -0400

    updated Toolbar

--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -1,26 +1,40 @@
 <template>
     <table>
-      <tbody>
-        <tr>
-            <td>
-                <input :value="displayRange">
-            </td>
-            <td>
-                <select>
-                <option>SUM</option>
-                <option>AVERAGE</option>
-                <option>MIN</option>
-                <option>MAX</option>
-                </select>
-            </td>
-            <td>
-                <img src="https://png.icons8.com/sigma/office/30" title="Sigma" width="30" height="30">
-            </td>
-            <td>
-                <input/>
-            </td>
-        </tr>
-      </tbody>
+        <tbody>
+            <tr>
+                <td>
+                    <input
+                        class="displayRange"
+                        :value="displayRange"
+                        placeholder="A1:A3"
+                    />
+                </td>
+                <td>
+                    <select>
+                        <option>SUM</option>
+                        <option>AVERAGE</option>
+                        <option>MIN</option>
+                        <option>MAX</option>
+                    </select>
+                </td>
+                <td>
+                    <div>
+                    <img
+                        src="https://png.icons8.com/sigma/office/30"
+                        title="Sigma"
+                        width="30"
+                        height="30">
+                    <span>=</span>
+                    </div>
+                </td>
+                <td>
+                    <input
+                        class="result"
+                        placeholder="89.2 + 99.0 + 92.7"
+                    />
+                </td>
+            </tr>
+        </tbody>
     </table>
 </template>
 <script>
@@ -38,6 +52,20 @@ export default {
     table {
         height: 150px;
         background: #eeeeee;
+    }
+    .displayRange {
+        font-size: 15px;
+        border: 1px solid #e3f2fd;
+        height: 30px;
+        width: 100px;
+        border-radius: 5px;
+    }
+    .result {
+        font-size: 15px;
+        border: 1px solid #e3f2fd;
+        height: 30px;
+        width: 250px;
+        border-radius: 5px;
     }
 </style>
 
